88ec8c2210302b78f175bc08824cae45becc33aa
prashansan99 <56554666+prashansan99@users.noreply.github.com>
2024-08-30 14:46:13 +0530

    Add files via upload

---
+++ Assignments/Java/Assignment_1/Number(Q1).java
@@ -0,0 +1,16 @@
+class Number
+{
+
+	public static void main(String args[])
+	{
+	    
+	    int N1 = -2;
+	    if(N1>0)
+		
+		System.out.println(N1 + "is a positive number");
+		else
+		System.out.println(N1 + "is a negative number");
+		
+		
+ 	}
+}		
