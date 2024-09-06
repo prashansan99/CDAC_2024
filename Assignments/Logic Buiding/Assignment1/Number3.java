52af9e30cbdf15ff5e284292b47794784f940db6
prashansan99 <56554666+prashansan99@users.noreply.github.com>
2024-09-06 11:28:07 +0530

    Add files via upload

---
+++ Assignments/Logic Buiding/Assignment1/Number3.java
@@ -0,0 +1,12 @@
+class Number3
+{
+	public static void main(String args[])
+	{
+	int N1 = 2;
+	if(N1%2==0)
+	System.out.println(N1 + "is an even number");
+	else
+	System.out.println(N1 + "is an odd number");
+	}
+
+}
