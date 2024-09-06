f5be676fabeb368561ad4e6bf880532924cac27e
prashansan99 <56554666+prashansan99@users.noreply.github.com>
2024-09-06 11:19:00 +0530

    Delete Assignments/Java/Logic Building directory

--- Assignments/Java/Logic Building/Number5.java
+++
@@ -1,8 +0,0 @@
-class Number5
-{
-	public static void main(String args[])
-	{
-	int side=5;
-	System.out.println("Area of the square is " + (side*side) + "cm^2");
-	}
-}
--- Assignments/Java/Logic Building/Number6.java
+++
@@ -1,9 +0,0 @@
-class Number6
-{
-	public static void main(String args[])
-	{
-	int length=4;
-	int breadth=2;
-	System.out.println("Area of the rectangle is " + (length*breadth)+ "cm^2");
-	}
-}
