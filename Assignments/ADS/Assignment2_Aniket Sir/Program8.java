989d0c9540e84482295c22d763b958b8de34d108
prashansan99 <56554666+prashansan99@users.noreply.github.com>
2024-10-14 08:41:32 +0530

    Add files via upload

---
+++ Assignments/ADS/Assignment2_Aniket Sir/Program8.java
@@ -0,0 +1,64 @@
+// Java code to demonstrate Difference Array 
+class Program8 { 
+	
+	 
+	static void initializeDiffArray(int A[], int D[]) 
+	{ 
+		
+		int n = A.length; 
+
+		D[0] = A[0]; 
+		D[n] = 0; 
+		for (int i = 1; i < n; i++) 
+			D[i] = A[i] - A[i - 1]; 
+	} 
+
+	
+	static void update(int D[], int l, int r, int x) 
+	{ 
+		D[l] += x; 
+		D[r + 1] -= x; 
+	} 
+
+	
+	static int printArray(int A[], int D[]) 
+	{ 
+		for (int i = 0; i < A.length; i++) { 
+			
+			if (i == 0) 
+				A[i] = D[i]; 
+
+			
+			else
+				A[i] = D[i] + A[i - 1]; 
+
+			System.out.print(A[i] + " "); 
+		} 
+		
+		System.out.println(); 
+		
+		return 0; 
+	} 
+	
+	public static void main(String[] args) 
+	{ 
+		
+		int A[] = { 10, 5, 20, 40 }; 
+		int n = A.length; 
+	
+		int D[] = new int[n + 1]; 
+		initializeDiffArray(A, D); 
+
+	
+		update(D, 0, 1, 10); 
+		printArray(A, D); 
+
+		
+		update(D, 1, 3, 20); 
+		update(D, 2, 2, 30); 
+		
+		printArray(A, D); 
+	} 
+} 
+
+
