67ea427045c3ac1e7a4eecffa90532e1aaef750a
prashansan99 <56554666+prashansan99@users.noreply.github.com>
2024-10-15 07:50:06 +0530

    Update Program10.java

--- Assignments/ADS/Assignment2_Aniket Sir/Program10.java
+++ Assignments/ADS/Assignment2_Aniket Sir/Program10.java
@@ -1,3 +1,39 @@
-class Program10
-{
+/*Explnation: In each iteration, shift the elements by one position to the left in a circular fashion (the first element becomes the last).
+  Perform this operation d times to rotate the elements to the left by d positions.
+// Java Program to left rotate the array by d positions
+// by rotating one element at a time
+
+import java.util.Arrays;
+
+class Program10 {
+    
+    // Function to left rotate array by d positions
+    static void rotateArr(int[] arr, int d) {
+        int n = arr.length;
+  
+        // Repeat the rotation d times
+        for (int i = 0; i < d; i++) {
+          
+            // Left rotate the array by one position
+            int first = arr[0];
+            for (int j = 0; j < n - 1; j++) {
+                arr[j] = arr[j + 1];
+            }
+            arr[n - 1] = first;
+        }
+    }
+
+    public static void main(String[] args) {
+        int[] arr = { 1, 2, 3, 4, 5, 6 };
+        int d = 2;
+
+        rotateArr(arr, d);
+
+        for (int i = 0; i < arr.length; i++)
+            System.out.print(arr[i] + " ");
+    }
 }
+/*Output: 3 4 5 6 1 2 
+  */
+/*Time complexity: O(n*d)
+  */
