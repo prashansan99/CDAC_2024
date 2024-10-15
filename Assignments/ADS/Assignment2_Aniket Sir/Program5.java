d031f99e588bec7b1960eb879af46c84dd51f776
prashansan99 <56554666+prashansan99@users.noreply.github.com>
2024-10-15 07:35:07 +0530

    Update Program5.java

--- Assignments/ADS/Assignment2_Aniket Sir/Program5.java
+++ Assignments/ADS/Assignment2_Aniket Sir/Program5.java
@@ -1,3 +1,6 @@
+/*Explanation:  we reverse the original array itself. In this method, we swap the elements of the array. The first element is swapped with the last element. The second element is swapped with the last but one element and so on. 
+For instance, consider array [1, 2, 3, …., n-2, n-1, n]. We swap 1 with n, 2 with n-1, 3 with n-2 and further. 
+	*/
 import java.util.Scanner;
 class Program5
 {
@@ -27,3 +30,8 @@ class Program5
 	}
 	
 }
+/*Output: 12345
+	54321
+	*/
+/*Time complexity: O(n)
+	*/
